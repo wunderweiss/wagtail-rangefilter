eac0b5482a6ba774dc280d12e17638444beb8f4a
Katherine Domingo <katherine.domingo@torchbox.com>
2023-06-16 10:33:23 +0800

    Linting

--- tests/testapp/wagtail_hooks.py
+++ tests/testapp/wagtail_hooks.py
@@ -1,4 +1,5 @@
 from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
+
 from wagtail_rangefilter.filters import DateRangeFilter, DateTimeRangeFilter
 
 from .models import TestModel
--- tests/urls.py
+++ tests/urls.py
@@ -2,8 +2,8 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from django.views.static import serve
-from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
+from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
 urlpatterns = [
